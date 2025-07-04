a96c1f53db305a2e7108ba042d8b92479f6923b2
prabinp101 <prabinp101@gmail.com>
2025-07-04 17:35:59 -0500

    learning to build python packages

---
+++ mymath/geometry.py
@@ -0,0 +1,11 @@
+import math
+"""
+module to do geometric caluclations of shapes
+"""
+
+def area_of_rectangle(length, width):
+    return length * width
+
+def area_of_circle(radius):
+    return math.pi * radius * radius
+
